4e6c1a771c8daedc301a279e05e22e0879e81f98
janburian <jburian@students.zcu.cz>
2023-05-17 10:42:39 +0200

    Changes in my_fft()

--- SP1/my_fft.m
+++ SP1/my_fft.m
@@ -1,18 +1,25 @@
-function X = my_fft(x)
-% Compute the FFT of a signal x using the Cooley-Tukey algorithm
+function [X] = my_fft(x)
+% Compute the FFT of a signal x using Cooley–Tukey FFT algorithm
 
 % Check if the input signal length is a power of 2
 N = length(x);
 if log2(N) ~= floor(log2(N))
-    error('Input signal length must be a power of 2');
+    error('Input signal length must be a power of 2.');
 end
 
-% Compute the FFT recursively
+% If signal has only one element
 if N == 1
     X = x;
 else
-    X_even = my_fft(x(1:2:N));
-    X_odd = my_fft(x(2:2:N));
-    twiddle = exp(-2j*pi*(0:N-1)/N);
-    X = [X_even + twiddle(1:N/2).*X_odd, X_even + twiddle(N/2+1:N).*X_odd];
+    % Extracting even and odd elements
+    even_elements = x(1:2:N);
+    odd_elements = x(2:2:N);
+    
+    % Compute the FFT recursively 
+    X_even = my_fft(even_elements);
+    X_odd = my_fft(odd_elements);
+    
+    % Complex part
+    twiddle_factor = exp(-2j*pi*(0:N-1)/N);
+    X = [X_even + twiddle_factor(1:N/2).*X_odd, X_even + twiddle_factor(N/2+1:N).*X_odd];
 end
